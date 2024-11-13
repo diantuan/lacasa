6d5959c33f3b87b7e0277ceadea8c535b379337f
DIANE MALLARI <diminibonboulash@gmail.com>
2024-11-13 18:26:12 +0800

    fixed the loading

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -2,14 +2,16 @@ import { useContext, useEffect, useRef, useState } from "react";
 import bgvid from '../../assets/LaCasaVideo.mp4';
 import { authContext } from "../Auth";
 import './login.css';
-import face from '../../assets/Money-Heist-Face-Mask-Background-PNG-Image.png';
+import facemask from '../../assets/face.png';
+import users from '../../data/users.json';
 
 const Login = () => {
 
   const [username, setUsername] = useState(null);
   const [password, setPassword] = useState(null);
   const videoref = useRef(null);
-  const {login, loginError} = useContext(authContext);
+  const [loginError, setLoginError] = useState(null);
+  const {login} = useContext(authContext);
 
   const [loading, setLoading] = useState(false);
 
@@ -18,16 +20,24 @@ const Login = () => {
     videoref.current.playbackRate = .7;
   }, [])
 
+
+
   const handleSubmit = (e)=>{
     e.preventDefault();
-    setLoading(true);
-    setTimeout(()=>{
-      setLoading(false)
-      login(username, password);
-    },5000)
+    let userFound = users.find(user=>user.username===username);
 
+      if(userFound && userFound.username===username && userFound.password===password){
+        
+        setLoading(true);
+        setTimeout(()=>{
+          setLoading(false)
+          login(username, password);
+        },5000)
     
-    
+      }
+      else{
+        setLoginError("Wrong username or password")
+      }
   }
 
   return ( <div>
@@ -58,8 +68,15 @@ const Login = () => {
       <button type="submit" className="loginButton">Log-in</button>
       {loginError && <p className="loginError">{loginError}</p>}
     </form>
-    {loading && <div><div className="loadingFace"><img src={face} alt="loading"></img> <div className="entering">Entering...</div></div>
-    <div className="loadingBox">...</div>
+
+    {loading && 
+    <div className="faceWrapper">
+     
+        <img className="loadingFace" src={facemask} alt="loading"></img> 
+        <img className="loadingFaceGray" src={facemask} alt="loading"></img> 
+        <div className="entering">Entering...</div>
+      
+     
     </div>}
   </div> );
 }
